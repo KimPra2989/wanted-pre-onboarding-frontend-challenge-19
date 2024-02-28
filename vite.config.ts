0f73c2da9b1508833bc2c6f89647fb135fe92af5
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-02-29 00:49:12 +0900

    /repo경로/

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: "https://kimpra2989.github.io/wanted-pre-onboarding-frontend-challenge-19/"
+  base: "/wanted-pre-onboarding-frontend-challenge-19/"
 });
